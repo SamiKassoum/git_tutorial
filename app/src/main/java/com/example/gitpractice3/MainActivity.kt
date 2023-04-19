6d917caa93fb3fcd8d4452cfdf1cd86ccd11e364
skassoum@gmail.com <skassoum@gmail.com>
2023-04-19 21:46:25 +0300

    Revert "this is our forth commit"
    This reverts commit 46e860cb2902d9787fe3ab70c0d30d5796b99136.

--- app/src/main/java/com/example/gitpractice3/MainActivity.kt
+++ app/src/main/java/com/example/gitpractice3/MainActivity.kt
@@ -8,6 +8,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("this is contained in my second commit")
-        println asdffdasf
     }
 }
